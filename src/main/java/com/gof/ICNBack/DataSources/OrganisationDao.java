b7c24e550829a2b98a47280f3fe8d035ded833ef
Astune <lys.2026485907@gmail.com>
2025-09-16 16:48:18 +1000

    add missing annotations for component

--- src/main/java/com/gof/ICNBack/DataSources/OrganisationDao.java
+++ src/main/java/com/gof/ICNBack/DataSources/OrganisationDao.java
@@ -1,9 +1,13 @@
 package com.gof.ICNBack.DataSources;
 
 import com.gof.ICNBack.Entity.Organisation;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Component
+@Transactional
 public class OrganisationDao {
     public List<Organisation> searchOrganisations(String location, List<String> filterParameters, String searchString, Integer skip, Integer limit) {
          return null;
